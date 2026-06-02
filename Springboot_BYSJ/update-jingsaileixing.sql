873276b3f7eb99ed83e4f10f2174e0f907727f95
Nanko <1367019690@qq.com>
2026-06-02 14:54:48 +0800

    添加项目说明文档和测试账号信息

--- Springboot_BYSJ/update-jingsaileixing.sql
+++
@@ -1,9 +0,0 @@
--- 更新zuopindafen表的jingsaileixing字段
-UPDATE zuopindafen 
-SET jingsaileixing = '测试竞赛' 
-WHERE id = 845773896086065152;
-
--- 验证更新结果
-SELECT id, xuehao, xueshengxingming, jingsaimingcheng, jingsaileixing, zuopinpingfen, gonghao 
-FROM zuopindafen 
-WHERE id = 845773896086065152;
--- archive/scripts/fix-password-now.sql
+++
@@ -1,4 +0,0 @@
-UPDATE xuesheng SET mima = '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi';
-UPDATE jiaoshi SET mima = '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi';
-SELECT xuehao, LEFT(mima, 15) as pwd_prefix FROM xuesheng LIMIT 3;
-SELECT gonghao, LEFT(mima, 15) as pwd_prefix FROM jiaoshi LIMIT 3;
